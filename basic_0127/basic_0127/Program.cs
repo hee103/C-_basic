550b35c1cf4b1ec2cd7eb43b063ba31345b59d11
hee103 <gmlgml3213@gmail.com>
2025-01-27 12:29:38 +0900

    1-3 변수와 자료형 1, 2

--- basic_0127/basic_0127/Program.cs
+++ basic_0127/basic_0127/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //1-2 프러그래밍 기본 요소
             Console.WriteLine("Hello, World!"); //출력 후 자동 개행
             Console.WriteLine("MY name is heewon");
             Console.WriteLine("C#");
@@ -20,6 +21,26 @@
             {
                 Console.WriteLine();
             }
+
+            //1-3 변수와 자료형 1, 2
+            //Console.Write("Enter your name: ");
+            //string name  = Console.ReadLine();
+            //Console.WriteLine("Hi, {0}!", name); // ctrl K + C로 자동 주석 +u로 주석 풀기
+
+            Console.Write("enter two numbers: ");
+            string input = Console.ReadLine();
+
+            string[] numbers = input.Split(' ');
+
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("The sum of {0} and {1} is {2}",num1, num2, sum);
+
+
+       
         }
 
     }
