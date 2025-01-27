1abaad8a5b00011b576f53c301122b2f44bb25b7
hee103 <gmlgml3213@gmail.com>
2025-01-27 11:44:13 +0900

    1-2  프로그래밍 기본 요소

--- basic_0127/basic_0127/Program.cs
+++ basic_0127/basic_0127/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"); //출력 후 자동 개행
+            Console.WriteLine("MY name is heewon");
+            Console.WriteLine("C#");
+
+            Console.WriteLine(10);
+            Console.WriteLine(3.141592); // double
+            Console.WriteLine(3+3);
+
+            Console.Write("Hello!"); //출력 후 자동 개행 X
+            Console.Write(" We are learning");
+            Console.Write("at TeamSparta");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine();
+            }
         }
+
     }
 }
