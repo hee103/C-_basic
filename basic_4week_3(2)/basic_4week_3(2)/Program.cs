384857384c25c46967e8514516a899f1eb801339
hee103 <gmlgml3213@gmail.com>
2025-02-03 10:28:08 +0900

    4주차 강의3

---
+++ basic_4week_3(2)/basic_4week_3(2)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace basic_4week_3_2_
+{
+    internal class Program
+    {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static void PrintMessaage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addFunc = Add;
+            int result = addFunc(3, 5);
+            Console.WriteLine("결과 : "+result);
+
+            Action<string> printAction = PrintMessaage;
+            printAction("Hello, world");
+        }
+    }
+}
---
+++ basic_4week_3/basic_4week_3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace basic_4week_3
+{
+    internal class Program
+    {
+        delegate void MyDelegate(string message);
+        static void Method1(string message)
+        {
+            Console.WriteLine("Method1:"+message);
+        }
+
+        static void Method2(string message)
+        {
+            Console.WriteLine("Method2:" + message);
+        }
+    
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Method1;
+            myDelegate += Method2;
+
+            myDelegate("Hello!");
+        }
+    }
+}
